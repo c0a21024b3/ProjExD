08267b41319eaba4398e2c2368ed3002391213cf
take <c0a21024b3@edu.teu.ac.jp>
2022-06-21 14:35:12 +0900

    ボタン作成

--- ex02/calc.py
+++ ex02/calc.py
@@ -1,18 +1,12 @@
 import tkinter as tk
 import tkinter.messagebox as tkm
 
-def num_click(event):
-    btn = event.widget
-    txt = btn["text"]
-    tkm.showinfo("お知らせ", f"これは{txt}です。")
-
 calc = tk.Tk()
 calc.title("電卓")
 calc.geometry("300x500")
 
 for i in range(10):
     button = tk.Button(calc, width=4, height=2, font=("Times New Roman", 30),text=f"{9-i}")
-    button.bind("<1>", num_click)
     button.grid(row=i//3, column=i%3)
 
 calc.mainloop()
