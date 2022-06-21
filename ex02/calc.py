4653ce7706800ede3a22178440e18d31dc8c0f39
take <c0a21024b3@edu.teu.ac.jp>
2022-06-21 14:50:50 +0900

    ＋ボタンの追加

--- ex02/calc.py
+++ ex02/calc.py
@@ -1,7 +1,7 @@
 import tkinter as tk
 import tkinter.messagebox as tkm
 
-def num_click(event):
+def button_click(event):
     btn = event.widget
     txt = btn["text"]
     entry.insert(tk.END, f"{txt}")
@@ -16,7 +16,11 @@ entry.grid(row=0, column=0, columnspan=3)
 
 for i in range(10):
     button = tk.Button(calc, width=4, height=2, font=("Times New Roman", 30),text=f"{9-i}")
-    button.bind("<1>", num_click)
+    button.bind("<1>", button_click)
     button.grid(row=i//3+1, column=i%3)
+
+button = tk.Button(calc, width=4, height=2, font=("Times New Roman", 30), text="+")
+button.bind("<1>", button_click)
+button.grid(row=4, column=1)
 
 calc.mainloop()
