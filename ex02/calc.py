27eabd8c9822c01c71ba5c8a66c0ba83286c45c1
take <c0a21024b3@edu.teu.ac.jp>
2022-06-21 14:43:04 +0900

    入力欄作成、ウィンドウサイズ変更

--- ex02/calc.py
+++ ex02/calc.py
@@ -8,11 +8,14 @@ def num_click(event):
 
 calc = tk.Tk()
 calc.title("電卓")
-calc.geometry("300x500")
+calc.geometry("300x600")
+
+entry = tk.Entry(calc, justify="right", width=10, font=("Times New Roman", 40))
+entry.grid(row=0, column=0, columnspan=3)
 
 for i in range(10):
     button = tk.Button(calc, width=4, height=2, font=("Times New Roman", 30),text=f"{9-i}")
     button.bind("<1>", num_click)
-    button.grid(row=i//3, column=i%3)
+    button.grid(row=i//3+1, column=i%3)
 
 calc.mainloop()
