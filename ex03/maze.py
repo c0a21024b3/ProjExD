e849c49e3d956a5de1243ff61002d977ee269f7a
take <c0a21024b3@edu.teu.ac.jp>
2022-06-28 16:59:33 +0900

    4限追加機能実装完了

--- ex03/maze.py
+++ ex03/maze.py
@@ -1,5 +1,9 @@
 import tkinter as tk
 import maze_maker
+import tkinter.messagebox as tkm
+
+yoko = 15 # 横のマス数
+tate = 9  # 縦のマス数
 
 def key_down(event):
     global key
@@ -13,7 +17,7 @@ def main_proc():
     global cx, cy, mx, my
     move = {"Up":[0, -1], "Down":[0, 1], "Right":[1, 0], "Left":[-1, 0]}
     try:
-        if maze[my+move[key][1]][mx+move[key][0]] == 0:
+        if maze[my+move[key][1]][mx+move[key][0]] != 1:
             mx += move[key][0]
             my += move[key][1]
     except:
@@ -22,7 +26,10 @@ def main_proc():
     cy = my * 100 + 50
     canvas.coords("kokaton", cx, cy)
     root.after(50, main_proc)
-
+    # ゴール時の処理
+    if maze[my][mx] == 2:
+        maze[my][mx] = 0
+        tkm.showinfo("Congratulations", "ゴールしました！")
 
 if __name__ == "__main__":
     root = tk.Tk()
@@ -34,9 +41,25 @@ if __name__ == "__main__":
                        background="black"
                        )
     
-    maze =  maze_maker.make_maze(15, 9)
+    maze =  maze_maker.make_maze(yoko, tate)
     maze_maker.show_maze(canvas, maze)
 
+    # スタートマス作成
+    canvas.create_rectangle(100, 100, 100+100, 100+100, 
+                                    fill="yellow")
+    # ゴールマス作成
+    if maze[tate-2][yoko-3] == 1:
+        maze[tate-2][yoko-2] = 2
+        canvas.create_rectangle(100*(yoko-2), 100*(tate-2), 100*(yoko-2)+100, 100*(tate-2)+100, 
+                                    fill="red")
+    else:
+        for y in range(tate-2, 0, -1):
+            if maze[y-1][yoko-2] == 1:
+                maze[y][yoko-2] = 2
+                canvas.create_rectangle(100*(yoko-2), 100*(y), 100*(yoko-2)+100, 100*(y)+100, 
+                                        fill="red")
+                break
+    
     kokaton = tk.PhotoImage(file="ex03/fig/0.png")
     cx, cy = 350, 450
     canvas.create_image(cx, cy, image=kokaton, tag="kokaton")
