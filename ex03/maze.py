4c9c5767496315b5101eb2246f3f32a66b5bb4cc
take <c0a21024b3@edu.teu.ac.jp>
2022-06-28 14:45:46 +0900

    main_proc関数追加

--- ex03/maze.py
+++ ex03/maze.py
@@ -8,6 +8,20 @@ def key_up(event):
     global key
     key = ""
 
+def main_proc():
+    global cx, cy
+    if key == "Up":
+        cy -= 20
+    elif key == "Down":
+        cy += 20
+    elif key == "Right":
+        cx += 20
+    elif key == "Left":
+        cx -= 20
+    canvas.coords("kokaton", cx, cy)
+    root.after(50, main_proc)
+    
+    
 if __name__ == "__main__":
     root = tk.Tk()
     root.title("迷えるこうかとん")
@@ -26,4 +40,6 @@ if __name__ == "__main__":
 
     root.bind("<KeyPress>", key_down)
     root.bind("<KeyRelease>", key_up)
+    main_proc()
+    
     root.mainloop()
