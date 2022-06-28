ab9e26ac64a2fcc56d26ea09a111764c14fb9b83
take <c0a21024b3@edu.teu.ac.jp>
2022-06-28 14:13:59 +0900

    Canvas作成

--- ex03/maze.py
+++ ex03/maze.py
@@ -3,4 +3,10 @@ import tkinter as tk
 if __name__ == "__main__":
     root = tk.Tk()
     root.title("迷えるこうかとん")
+    canvas = tk.Canvas(root,
+                       width=1500,
+                       height=900,
+                       background="black"
+                       )
+    canvas.pack()
     root.mainloop()
