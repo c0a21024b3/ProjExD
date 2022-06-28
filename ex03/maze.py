299225acf04dd67bae48013fdb5151889c3d9c85
take <c0a21024b3@edu.teu.ac.jp>
2022-06-28 14:19:02 +0900

    こうかとん表示

--- ex03/maze.py
+++ ex03/maze.py
@@ -3,10 +3,15 @@ import tkinter as tk
 if __name__ == "__main__":
     root = tk.Tk()
     root.title("迷えるこうかとん")
+
     canvas = tk.Canvas(root,
                        width=1500,
                        height=900,
                        background="black"
                        )
+    kokaton = tk.PhotoImage(file="ex03/fig/0.png")
+    cx, cy = 300, 400
+    canvas.create_image(cx, cy, image=kokaton, tag="kokaton")
     canvas.pack()
+
     root.mainloop()
