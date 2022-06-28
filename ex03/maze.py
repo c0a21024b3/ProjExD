c6ce2b77049ed3e54bf7442d0f02f82478e0bc78
take <c0a21024b3@edu.teu.ac.jp>
2022-06-28 14:28:06 +0900

    key_up関数追加

--- ex03/maze.py
+++ ex03/maze.py
@@ -4,6 +4,10 @@ def key_down(event):
     global key
     key = event.keysym
 
+def key_up(event):
+    global key
+    key = ""
+
 if __name__ == "__main__":
     root = tk.Tk()
     root.title("迷えるこうかとん")
@@ -21,4 +25,5 @@ if __name__ == "__main__":
     key = ""
 
     root.bind("<KeyPress>", key_down)
+    root.bind("<KeyRelease>", key_up)
     root.mainloop()
