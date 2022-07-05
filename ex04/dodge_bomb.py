2cc6f64a2898e5680a5a79d9cbc2ac6c0599cabd
take <c0a21024b3@edu.teu.ac.jp>
2022-07-05 14:48:32 +0900

    5問目

--- ex04/dodge_bomb.py
+++ ex04/dodge_bomb.py
@@ -1,5 +1,6 @@
 import pygame as pg
 import sys
+import random
 
 def main():
     clock = pg.time.Clock()
@@ -7,6 +8,7 @@ def main():
 
     pg.display.set_caption("逃げろ！こうかとん！")
     screen = pg.display.set_mode((1600, 900))
+    screen_rect = screen.get_rect()
 
     bg = pg.image.load("fig/pg_bg.jpg")
     bg_rect = bg.get_rect()
@@ -16,6 +18,12 @@ def main():
     koka = pg.transform.rotozoom(koka, 0, 2.0)
     koka_rect = koka.get_rect()
     koka_rect.center = 900, 400
+
+    bomb = pg.Surface((20, 20))
+    bomb.set_colorkey((0, 0, 0))
+    pg.draw.circle(bomb, (255, 0, 0), (10, 10), 10)
+    bomb_rect = bomb.get_rect()
+    bomb_rect.center = random.randint(0,screen_rect.width),                        random.randint(0, screen_rect.height)
 
     while True:
         screen.blit(bg, bg_rect)
@@ -36,7 +44,7 @@ def main():
             koka_rect.centerx += 1
         screen.blit(koka, koka_rect)
 
-
+        screen.blit(bomb, bomb_rect)
 
         pg.display.update()
         clock.tick(1000)
