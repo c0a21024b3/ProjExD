08de095aa4db20ca45a44e4fbb8dd724b2c86971
take <c0a21024b3@edu.teu.ac.jp>
2022-07-05 14:37:29 +0900

    4問目

--- ex04/dodge_bomb.py
+++ ex04/dodge_bomb.py
@@ -12,18 +12,31 @@ def main():
     bg_rect = bg.get_rect()
     screen.blit(bg, bg_rect)
 
-    koka = pg.image.load("fig/5.png")
+    koka = pg.image.load("fig/3.png")
     koka = pg.transform.rotozoom(koka, 0, 2.0)
     koka_rect = koka.get_rect()
     koka_rect.center = 900, 400
 
     while True:
         screen.blit(bg, bg_rect)
-        screen.blit(koka, koka_rect)
 
         for event in pg.event.get():
             if event.type == pg.QUIT:
                 return
+            
+        key_states = pg.key.get_pressed()# キー入力状態の辞書
+        # 上下左右に動かす
+        if key_states[pg.K_UP]:
+            koka_rect.centery -= 1
+        if key_states[pg.K_DOWN]:
+            koka_rect.centery += 1
+        if key_states[pg.K_LEFT]:
+            koka_rect.centerx -= 1
+        if key_states[pg.K_RIGHT]:
+            koka_rect.centerx += 1
+        screen.blit(koka, koka_rect)
+
+
 
         pg.display.update()
         clock.tick(1000)
