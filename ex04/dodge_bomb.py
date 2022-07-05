80a22912c52353cea5614ec863512c897cf6cbf9
take <c0a21024b3@edu.teu.ac.jp>
2022-07-05 17:43:22 +0900

    コメント追加 #9

--- ex04/dodge_bomb.py
+++ ex04/dodge_bomb.py
@@ -19,33 +19,35 @@ def main():
     koka_rect = koka.get_rect()
     koka_rect.center = 900, 400
 
-    #爆弾を4つ生成　内3つは画面外
+    #1つ目の爆弾を生成
     bomb1 = pg.Surface((20, 20))
     bomb1.set_colorkey((0, 0, 0))
     pg.draw.circle(bomb1, (255, 0, 0), (10, 10), 10)
     bomb1_rect = bomb1.get_rect()
-    bomb1_rect.center = random.randint(0,screen_rect.width),                        random.randint(0, screen_rect.height)
-    vx1, vy1 = 1, 1
+    bomb1_rect.center = random.randint(0,screen_rect.width), random.randint(0, screen_rect.height)
+    vx1, vy1 = 1, 1 # 縦横方向の速度
 
+    # 残りの爆弾は画面外に生成
     bomb2 = pg.Surface((20, 20))
     bomb2.set_colorkey((0, 0, 0))
     pg.draw.circle(bomb2, (255, 0, 0), (10, 10), 10)
     bomb2_rect = bomb2.get_rect()
-    bomb2_rect.center = random.randint(-50,-50),                        random.randint(0, screen_rect.height)
-    vx2, vy2 = 0, 0
+    bomb2_rect.center = -50,-50 # 初期位置を画面外に設定
+    vx2, vy2 = 0, 0 # 速度を0で初期化
 
+    #以下同様
     bomb3 = pg.Surface((20, 20))
     bomb3.set_colorkey((0, 0, 0))
     pg.draw.circle(bomb3, (255, 0, 0), (10, 10), 10)
     bomb3_rect = bomb3.get_rect()
-    bomb3_rect.center = random.randint(-50,-50),                        random.randint(0, screen_rect.height)
+    bomb3_rect.center = -50,-50
     vx3, vy3 = 0, 0
 
     bomb4 = pg.Surface((20, 20))
     bomb4.set_colorkey((0, 0, 0))
     pg.draw.circle(bomb4, (255, 0, 0), (10, 10), 10)
     bomb4_rect = bomb4.get_rect()
-    bomb4_rect.center = random.randint(-50,-50),                        random.randint(0, screen_rect.height)
+    bomb4_rect.center = -50,-50
     vx4, vy4 = 0, 0
 
     bombs = [(bomb1, bomb1_rect), (bomb2, bomb2_rect), (bomb3, bomb3_rect),(bomb4, bomb4_rect)] # 爆弾のリスト
@@ -53,10 +55,11 @@ def main():
 
     while True:
         clock.tick(1000)
-        time = pg.time.get_ticks()
+        time = pg.time.get_ticks() # 経過時間を取得
 
         screen.blit(bg, bg_rect)
 
+        # 経過時間の表示
         font = pg.font.Font(None, 80)
         txt = font.render(str(time/1000)[:-1], True, (0, 0, 0))
         screen.blit(txt, (760, 40))
@@ -91,9 +94,9 @@ def main():
         screen.blit(bomb1, bomb1_rect)
 
         if 19950 <= time <= 20000: # 経過時間20秒なら 誤差込み
-            vx2 = -1 * vx1 # 横は1つ目と逆方向で1.5倍速
+            vx2 = -1 * vx1 # 横は1つ目と逆
             vy2 = 2 * vy1 # 縦は1つ目の倍速
-            # 1つ目から分裂するように描画
+            # 1つ目の位置に移動
             bomb2_rect.centerx = bomb1_rect.centerx
             bomb2_rect.centery = bomb1_rect.centery
         bomb2_rect.move_ip(vx2, vy2)
@@ -107,7 +110,7 @@ def main():
         if 39950 <= time <= 40000: # 経過時間40秒なら 誤差込み
             vx3 = 2 * vx1 # 横は1つ目の倍速
             vy3 = -1 * vy1 # 縦は1つ目と逆
-            # 1つ目から分裂するように描画
+            # 1つ目の位置に移動
             bomb3_rect.centerx = bomb1_rect.centerx
             bomb3_rect.centery = bomb1_rect.centery
         bomb3_rect.move_ip(vx3, vy3)
@@ -121,7 +124,7 @@ def main():
         if 59950 <= time <= 60000: # 経過時間60秒なら 誤差込み
             vx4 = -2 * vx1 # 横は1つ目と逆で倍速
             vy4 = 2 * vy1 # 縦は1つ目の倍速
-            # 1つ目から分裂するように描画
+            # 1つ目の位置に移動
             bomb4_rect.centerx = bomb1_rect.centerx
             bomb4_rect.centery = bomb1_rect.centery
         bomb4_rect.move_ip(vx4, vy4)
@@ -132,6 +135,7 @@ def main():
             vy4 *= -1
         screen.blit(bomb4, bomb4_rect)
         
+        # 全ての爆弾について接触判定を行う
         for x in bombs:
             if koka_rect.colliderect(x[1]):
                 return
